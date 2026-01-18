6b32f036f007563939597dd68c9a2854e2042844
Rafitos <rafaelsevla@gmail.com>
2026-01-18 02:13:18 -0300

    rafactor string calculator

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,38 +2,27 @@
 
 class NumeroNegativoError < StandardError; end
 
-def ignore_thousands(number)
-  return '' if number.to_i >= 1000
-
-  number
+def extract_numbers(text)
+  text.scan(/-?\d+/).map(&:to_i)
 end
 
-def raise_if_negative_numbers(list)
-  negatives_check = list.join(',').scan(/-\d{1}/)
-  raise NumeroNegativoError, "negatives not allowed: #{negatives_check.join(', ')}" if negatives_check.length.positive?
-end
+def raise_if_negative_numbers(numbers)
+  negatives = numbers.select(&:negative?)
 
-def split_and_sum(text)
-  splited = text.split(',')
-  raise_if_negative_numbers(splited)
-  with_thousands_ignored = splited.map { |num| ignore_thousands(num) }
+  return if negatives.empty?
 
-  scan = with_thousands_ignored.join.scan(/\d{1}/)
-  scan.map(&:to_i).sum
-end
-
-def define_when_split_and_sum?(text)
-  text.include?(',') or text.include?(';')
+  raise NumeroNegativoError,
+        "negatives not allowed: #{negatives.join(', ')}"
 end
 
 def add(text)
-  begin # rubocop:disable Style/RedundantBegin
-    return 0 if text == ''
+  return 0 if text.empty?
 
-    return split_and_sum(text) if define_when_split_and_sum?(text)
-
-    text.to_i
-  rescue NumeroNegativoError => e
-    e.message
-  end
+  numbers = extract_numbers(text)
+  raise_if_negative_numbers(numbers)
+  numbers.reject { |n| n > 1000 }.sum
+rescue NumeroNegativoError => e
+  e.message
 end
+
+add('100,2,2')
